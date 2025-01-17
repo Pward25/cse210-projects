d5832950f6552c7dcd91d3552875bf10f1babc7c
Parker Ward <war19040@byui.edu>
2025-01-16 21:49:47 -0700

    Updated prep assignment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    
+        Console.Write("Input grade percentage: ");
+        string userGrade = Console.ReadLine();
+        int number = int.Parse(userGrade);
+
+
+
+        if (number >= 90)
+        {
+            Console.WriteLine("Letter Grade: A");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Letter Grade: B");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Letter Grade: C");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Letter Grade: D");
+        }
+        else
+        {
+            Console.WriteLine("Letter Grade: F");
+        }
+
+        if (number < 70)
+        {
+            Console.WriteLine("Failed! Better luck next time!");
+        }
+        else
+        {
+            Console.WriteLine("Passed. Great work!!");
+        }
     }
-}
+
+}  
